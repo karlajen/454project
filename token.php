607f742e7d0f74399fad2b4b610550b1a0a27bcf
karlajen <klugo1@csu.fullerton.edu>
2018-12-13 10:17:49 -0800

    Create token.php

---
+++ token.php
@@ -0,0 +1,13 @@
+<?php
+class Token{
+        public static function generate(){
+        return $_SESSION['token']= base64_encode(openssl_random_pseudo_bytes(32));
+
+        }
+        public static function check($token){
+        }
+
+
+}
+?>
+
